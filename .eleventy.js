d2e50dcaaf495064190129ac40e9f98878ae5e59
Piper Haywood <piper.haywood@gmail.com>
2019-09-24 14:14:59 +0100

    Adjust eleventy fconfig

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,61 @@
+const { DateTime } = require("luxon");
+const CleanCSS = require("clean-css");
+const UglifyJS = require("uglify-es");
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.setBrowserSyncConfig({
     notify: true
   });
+
+  // Minify CSS
+  eleventyConfig.addFilter("cssmin", function(code) {
+    return new CleanCSS({}).minify(code).styles;
+  });
+
+  // Minify JS
+  eleventyConfig.addFilter("jsmin", function(code) {
+    let minified = UglifyJS.minify(code);
+    if (minified.error) {
+      console.log("UglifyJS error: ", minified.error);
+      return code;
+    }
+    return minified.code;
+  });
+
+  // Date formatting (machine readable)
+  eleventyConfig.addFilter("machineDate", dateObj => {
+    return DateTime.fromJSDate(dateObj).toFormat("yyyy-MM-dd");
+  });
+
+  // Date formatting (human readable)
+  eleventyConfig.addFilter("readableDate", dateObj => {
+    return DateTime.fromJSDate(dateObj).toFormat("dd LLL yyyy");
+  });
+
+  // only content in the `posts/` directory
+  eleventyConfig.addCollection("posts", function(collection) {
+    return collection.getAllSorted().filter(function(item) {
+      return item.inputPath.match(/^\.\/posts\//) !== null;
+    });
+  });
+
+  // Copy the fonts
+  eleventyConfig.addPassthroughCopy({ "src/_includes/assets/fonts": "fonts" });
+
+  return {
+    templateFormats: ["md", "njk", "html", "liquid", "woff", "woff2"],
+
+    pathPrefix: "/",
+
+    markdownTemplateEngine: "liquid",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    // passthroughFileCopy: true,
+    dir: {
+      input: "src/.",
+      includes: "_includes",
+      data: "_data",
+      output: "_dist"
+    }
+  };
 };
